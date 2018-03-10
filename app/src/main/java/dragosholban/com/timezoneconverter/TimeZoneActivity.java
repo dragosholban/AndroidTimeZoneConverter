338756e0a2f5389b16f01e31f1dc8904f2e14295
Dragos Holban <dragos@intelligentbee.com>
2018-03-10 15:48:52 +0200

    Build a List of Time Zones

--- app/src/main/java/dragosholban/com/timezoneconverter/TimeZoneActivity.java
+++ app/src/main/java/dragosholban/com/timezoneconverter/TimeZoneActivity.java
@@ -2,6 +2,12 @@ package dragosholban.com.timezoneconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.TimeZone;
 
 public class TimeZoneActivity extends AppCompatActivity {
 
@@ -9,5 +15,10 @@ public class TimeZoneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_zone);
+
+        ListView listView = findViewById(R.id.listView);
+        ArrayList<String> timezones = new ArrayList<>(Arrays.asList(TimeZone.getAvailableIDs()));
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, timezones);
+        listView.setAdapter(adapter);
     }
 }
