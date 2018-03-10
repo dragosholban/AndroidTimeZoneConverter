7c5f9d67e40e04ddcb4b0612016db678b32185e4
Dragos Holban <dragos@intelligentbee.com>
2018-03-10 13:26:03 +0200

    Set the Time

--- app/src/main/java/dragosholban/com/timezoneconverter/MainActivity.java
+++ app/src/main/java/dragosholban/com/timezoneconverter/MainActivity.java
@@ -2,12 +2,42 @@ package dragosholban.com.timezoneconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    Date localDate = new Date();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        final TextView userTime = findViewById(R.id.userTime);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean fromUser) {
+                userTime.setText((seekBar.getProgress() < 10 ? "0" + Integer.toString(seekBar.getProgress()) : Integer.toString(seekBar.getProgress())) + ":00");
+                localDate.setHours(seekBar.getProgress());
+                if (fromUser) {
+                    localDate.setMinutes(0);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar.setProgress(localDate.getHours());
     }
 }
